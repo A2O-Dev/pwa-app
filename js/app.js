95ba284c28139b8686a35816b2ac175e1cf9ad07
David Almaraz <cancioalmaraz@gmail.com>
2022-04-01 17:23:27 -0400

    Added button to install app

--- js/app.js
+++ js/app.js
@@ -3,10 +3,28 @@ if (!!navigator.serviceWorker) {
   navigator.serviceWorker.register('/serviceWorker.js')
 }
 
+let installBtn = $('#install-btn');
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  deferredPrompt = e;
+});
+
+installBtn.on('click', async () => {
+  if (deferredPrompt !== null) {
+    deferredPrompt.prompt();
+    const {outcome} = await deferredPrompt.userChoice;
+    if (outcome === 'accepted') {
+      deferredPrompt = null;
+    }
+  }
+});
+
 window.addEventListener('appinstalled', () => {
-  $('#install-btn').hide();
+  installBtn.hide();
 });
 
 if (window.matchMedia('(display-mode: standalone)').matches) {
-  $('#install-btn').hide();
+  installBtn.hide();
 }
