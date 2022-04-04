6fc03fac66bc20589bd7d0ea1f837caa639af30f
David Almaraz <cancioalmaraz@gmail.com>
2022-04-04 13:44:02 -0400

    Improve listeners

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 if (!!navigator.serviceWorker) {
   navigator.serviceWorker.register('/service-worker.js')
 }
@@ -10,18 +9,55 @@ let labelTextArea = $('#label-textarea');
 
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  deferredPrompt = e;
-});
+let listening = false;
+let interval;
 
-installBtn.on('click', async () => {
+installBtn.on('click', () => {
   if (deferredPrompt !== null) {
     deferredPrompt.prompt();
-    const {outcome} = await deferredPrompt.userChoice;
-    if (outcome === 'accepted') {
-      deferredPrompt = null;
+
+    deferredPrompt.userChoice
+      .then(({outcome}) => {
+        if (outcome === 'accepted') {
+          deferredPrompt = null;
+        }
+      });
+  }
+});
+
+listenBtn.on('click', () => {
+  listening = !listening;
+
+  if (listening) {
+    listenBtn.removeClass('btn-primary');
+    listenBtn.addClass('btn-success');
+    interval = setInterval(() => {
+      textArea.focus();
+    }, 100);
+    listenBtn.text('Listening...');
+    labelTextArea.text('Listening...');
+  } else {
+    listenBtn.removeClass('btn-success');
+    listenBtn.addClass('btn-primary');
+    clearInterval(interval);
+    listenBtn.text('Start to Listen');
+    labelTextArea.text('Stopped');
+  }
+})
+
+window.addEventListener('keyup', ({key}) => {
+  if (!['Shift', 'Control', 'AltGraph', 'Alt', 'CapsLock', 'Tab', 'Backspace', 'Escape'].includes(key)) {
+    if (key === 'Enter') {
+      textArea.val(`${textArea.val()}\n`);
+    } else {
+      textArea.val(`${textArea.val()}${key}`);
     }
   }
+})
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
 });
 
 window.addEventListener('appinstalled', () => {
@@ -31,39 +67,3 @@ window.addEventListener('appinstalled', () => {
 if (window.matchMedia('(display-mode: standalone)').matches) {
   installBtn.hide();
 }
-
-let listening = false;
-let interval;
-
-$(function () {
-
-  listenBtn.on('click', () => {
-    listening = !listening;
-
-    if (listening) {
-      listenBtn.removeClass('btn-primary');
-      listenBtn.addClass('btn-success');
-      interval = setInterval(() => {
-        textArea.focus();
-      }, 100);
-      listenBtn.text('Listening...');
-      labelTextArea.text('Listening...');
-    } else {
-      listenBtn.removeClass('btn-success');
-      listenBtn.addClass('btn-primary');
-      clearInterval(interval);
-      listenBtn.text('Start to Listen');
-      labelTextArea.text('Stopped');
-    }
-  })
-
-  $(window).keyup(({key}) => {
-    if (!['Shift', 'Control', 'AltGraph', 'Alt', 'CapsLock', 'Tab', 'Backspace', 'Escape'].includes(key)) {
-      if (key === 'Enter') {
-        textArea.val(`${textArea.val()}\n`);
-      } else {
-        textArea.val(`${textArea.val()}${key}`);
-      }
-    }
-  });
-});
